9dd1090c9a83b2a8863101be2698fba23402e5a7
Paul Reekers <info@paulreekers.nl>
2016-04-26 18:45:36 +0200

    Feature: Settingup the fishpi datacenter

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Welcome at the Fishpi Datacenter</div>
+
+                <div class="panel-body">
+                    <h1>How to create your own datacenter fishtank!</h1>
+                    <h3>And keep the fishies happy.</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
