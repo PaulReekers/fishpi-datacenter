16977b8e37849dbe4dc7c2d4aa52eb8656b0b82a
Paul Reekers <info@paulreekers.nl>
2016-05-22 11:03:36 +0200

    Feature: Send a summary of air and water temperature via twitter
    Through the cronjob we send a daily tweet with the connected app.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,24 +7,13 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
+
     protected $commands = [
-        // Commands\Inspire::class,
+        'App\Console\Commands\DailyTweet'
     ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('twitter:postDailyTweet')->dailyAt('08:00');
     }
 }
